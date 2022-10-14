13c199f25b0d747a7da6d8517ad97921d0585274
Vic Kuo <eun.hru@gmail.com>
2022-10-14 17:11:29 +0800

    20221014

--- routes/stripe.js
+++ routes/stripe.js
@@ -6,7 +6,7 @@ router.post("/payment", (req, res) => {
     stripe.charges.create({
         source: req.body.tokenId,
         amount: req.body.amount,
-        currency: "twd"
+        currency: "usd"
     }, (stripeErr, stripeRes) => {
         if (stripeErr) {
             res.status(500).json(stripeErr)
