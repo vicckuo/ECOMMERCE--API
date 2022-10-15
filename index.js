709625be6751a3cd4ce2faa22ddd14b7ae09c259
Vic Kuo <eun.hru@gmail.com>
2022-10-15 21:26:01 +0800

    stripe

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+dotenv.config()
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
-// const stripeRoute = require("./routes/stripe")
+const stripeRoute = require("./routes/stripe")
 const cors = require("cors")
 
-dotenv.config()
+
+
 
 mongoose
     .connect(process.env.MONGO_URL)
@@ -26,7 +28,7 @@ app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
 app.use("/api/carts", cartRoute)
 app.use("/api/orders", orderRoute)
-// app.use("/api/checkout", stripeRoute)
+app.use("/api/checkout", stripeRoute)
 
 
 app.listen(process.env.PORT || 5500, () => {
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,19 +1,24 @@
 const router = require("express").Router()
-const stripe = require("stripe")(process.env.STRIPE_KEY)
-
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY
+const stripe = require("stripe")(KEY);
+// const Stripe = require("stripe")
+// const stripe = Stripe('sk_test_51LsNrZCbgMrWnfY4ojJ0e4hx8XzEjQ2Prtd1GhCe8lBOZ0WL7SbiJudxMOyWYz2zVz8OgdIu0lqW5Xev2zNjsGTg00Qv6EzbiV')
 
 router.post("/payment", (req, res) => {
-    stripe.charges.create({
-        source: req.body.tokenId,
-        amount: req.body.amount,
-        currency: "usd"
-    }, (stripeErr, stripeRes) => {
-        if (stripeErr) {
-            res.status(500).json(stripeErr)
-        } else {
-            res.status(200).json(stripeRes)
-        }
-    })
+    stripe.charges.create(
+        {
+            source: req.body.tokenId,
+            amount: req.body.amount,
+            currency: "usd"
+        },
+        (stripeErr, stripeRes) => {
+            if (stripeErr) {
+                res.status(500).json(stripeErr)
+            } else {
+                res.status(200).json(stripeRes)
+            }
+        })
 })
 
 module.exports = router
